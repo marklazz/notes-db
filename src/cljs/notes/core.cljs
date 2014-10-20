22594ce7f3b1c188b0a5aba830285efcd689c352
Marcelo Giorgi <marklazz.uy@gmail.com>
2014-10-19 23:58:07 -0200

    Keep text when using tab in either direction

--- src/cljs/notes/core.cljs
+++ src/cljs/notes/core.cljs
@@ -14,7 +14,7 @@
 (defn now [] (new js/Date))
 (def custom-formatter (time-format/formatter "dd/MM/yyyy"))
 (def app-title (str "Notes " (time-format/unparse custom-formatter (time-core/date-time (.getFullYear (now)) (+ 1 (.getMonth (now))) (.getDate (now))))))
-(def app-state (atom {:notes [] :tab 0 }))
+(def app-state (atom {:entered [] :current { :tab 0 :title "" }}))
 
 (defn note-view  [note owner]
   (reify
@@ -38,6 +38,7 @@
    39 "right"
    40 "down"
    46 "del"
+   32 "space"
    186 ";"})
  
 (defn event-modifiers
@@ -72,8 +73,20 @@
     (if (and key (not (empty? key)) (not (some #{key} mod-keys)))
       (string/join "+" (conj mods key)))))
 
+(defn alphanumeric [s]
+  (re-find #"^[a-z0-9]+$" s))
+
 (defn handle-new-note-keydown [e app owner]
-  (let [code (event->key e)]
+  (let [code (event->key e)
+        which (.-which e)
+        key (.toLowerCase (js/String.fromCharCode which))]
+  (if (or (alphanumeric key) (== code "space"))
+  (do
+    (om/transact! app [:current :title]
+      #(str % key)
+      [:title nil])
+  )
+  (do
   (case code
     "enter"
     (do
@@ -82,24 +95,27 @@
         (let [new-note {:id (guid)
                         :title (.-value new-field)
                         :status "pending"
-                        :tab (:tab @app) }]
-          (om/transact! app :notes
+                        :tab (get-in @app [:current :tab])}]
+          (om/transact! app :entered
             #(conj % new-note)
-            [:create new-note]))
-        (set! (.-value new-field) "")))
+            [:create new-note])
+          (om/transact! app [:current :title]
+            (fn [_] "")
+            [:clean-current]))
+        ))
         false)
     "tab" (do
-      (om/transact! app :tab
+      (om/transact! app [:current :tab]
         #(+ % 1)
         [:tab nil])
       false)
     "shift+tab" (do
-      (om/transact! app :tab
+      (om/transact! app [:current :tab]
         #(- % 1)
         [:tab nil])
       false)
     nil
-    )))
+    )))))
 
 (defn focus-on-input [] (-> js/document (.querySelector "input") (.focus)))
 
@@ -121,7 +137,7 @@
       (let [delete (om/get-state owner :delete)]
         (go (loop []
               (let [note (<! delete)]
-                (om/transact! app :notes
+                (om/transact! app :entered
                   (fn [xs] (vec (remove #(= note %) xs))))
                 (recur))))))
     om/IRenderState
@@ -129,14 +145,15 @@
       (dom/div nil
         (dom/h1 nil app-title)
         (apply dom/ul nil
-          (om/build-all note-view (:notes app)
+          (om/build-all note-view (:entered app)
             {:init-state state}))
         (dom/ul nil
           (indent-element
             (dom/li nil (dom/input #js {:type "text" :ref "newNote"
               :placeholder "Enter note here..."
               :onKeyDown #(handle-new-note-keydown % app owner)
-            })) (:tab app)
+              :value (get-in app [:current :title])
+            })) (get-in app [:current :tab])
           )
           ;(dom/button #js {:onClick #(add-note app owner)} "Add note")
         )))))
@@ -148,7 +165,7 @@
   (let [new-note (-> (om/get-node owner "newNote")
                         .-value)]
     (when new-note
-      (om/transact! app :notes #(conj % new-note)))))
+      (om/transact! app :entered #(conj % new-note)))))
 
 (om/root
   (fn [app owner]
